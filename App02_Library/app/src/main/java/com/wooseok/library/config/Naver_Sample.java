81a689b1c257896c5adb538db3387826fabc3519
wooseok0727 <crash2@naver.com>
2021-08-06 11:08:21 +0900

    8월 6일

--- App02_Library/app/src/main/java/com/wooseok/library/config/Naver_Sample.java
+++ App02_Library/app/src/main/java/com/wooseok/library/config/Naver_Sample.java
@@ -1,7 +1,7 @@
 package com.wooseok.library.config;
 
 // Naver Open API 연동을 위한 ID와 비밀번호!!
-public class Naver {
+public class Naver_Sample {
 
     public static String CLIENT_ID = "YOUR NAVER CLIENT ID";
     public static String CLIENT_SECRET = "YOUR NAVER CLIENT SECRET";
