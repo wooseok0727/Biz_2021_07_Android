d2f54975d895c5b478902bb9dfc4ae52153577b9
wooseok0727 <crash2@naver.com>
2021-08-10 09:47:22 +0900

    8월 10일

---
+++ App03_Library/app/src/main/java/com/wooseok/library/config/NaverAPI_Sample.java
@@ -0,0 +1,14 @@
+package com.wooseok.library.config;
+
+/**
+ * Naver OpenAPI에 접근하기 위한 정보를 포함하는 클래스
+ * Naver OpenAPI 관련 정보를 변경한 후
+ * NaverAPI 로 변경하여 프로젝트를 빌드
+ */
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID = "YOUR NAVER API ID";
+    public static final String CLIENT_SECRET = "YOUR NAVER API SECRET";
+    public static final String NAVER_BOOK_URL = "https://openapi.naver.com/v1/search/book.json";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+}
