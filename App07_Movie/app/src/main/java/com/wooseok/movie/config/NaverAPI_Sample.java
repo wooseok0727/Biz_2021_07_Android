2dd740bcae4dd5f2ab9d3d7937f37e6b9c18cf5d
wooseok0727 <crash2@naver.com>
2021-08-17 15:25:21 +0900

    8월 17일

---
+++ App07_Movie/app/src/main/java/com/wooseok/movie/config/NaverAPI_Sample.java
@@ -0,0 +1,13 @@
+package com.wooseok.movie.config;
+
+/*
+    Naver Open API 를 등록한 후
+    ID, SECRET을 작성하여
+    클래스를 NaverAPI로 변경한 후 프로젝트를 build 하시오
+ */
+public class NaverAPI_Sample {
+
+    public static final String NAVER_CLIENT_ID = "YOUR NAVER API ID";
+    public static final String NAVER_CLIENT_SECRET = "YOUR NAVER API SECRET";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+}
